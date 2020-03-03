dfeab9796d60bf7046b24cc162a2e305e8f21bf2
rayhantif <rayhan.zharfan.tif18@polban.ac.id>
2020-03-03 10:13:21 +0700

    back to localhost:5000

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6ca649e926f6316b5ef0d7ba63d9330.js"
+  "/precache-manifest.4611f4dd70f16c088da04c63f531423e.js"
 );
 
 self.addEventListener('message', (event) => {
