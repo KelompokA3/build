51430e7fef0317cd54f620d4ccb631362b9acc69
rayhantif <rayhan.zharfan.tif18@polban.ac.id>
2020-03-09 02:47:29 +0700

    revisi 5

--- precache-manifest.580fe8a196269ceff0e7241787983890.js
+++ precache-manifest.580fe8a196269ceff0e7241787983890.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f89c5da1034a7e2c54b67e3ddf8b78cf",
+    "revision": "75f59a91d8a6126f1d58da3394acbb74",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.6547dd70.chunk.css"
   },
   {
-    "revision": "e3a03738f3153c1ce526",
+    "revision": "5b2069597489fe8ade4d",
     "url": "/static/css/main.d6c2332f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.cd3b214c.chunk.js"
   },
   {
-    "revision": "e3a03738f3153c1ce526",
-    "url": "/static/js/main.1c5c784b.chunk.js"
+    "revision": "5b2069597489fe8ade4d",
+    "url": "/static/js/main.574311e1.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
